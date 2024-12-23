d5fc731e57ca8c5e2cb7ce53628c4a068190d142
gurmeet1011 <gurmeetrathorjnv1012@gmail.com>
2024-12-23 12:19:18 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,12 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+// App.js
+import React from 'react';
+
+export default function App() {
+    return (
+      <div id="main">
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+      </div>
+    );
+  }
+
 
